728f2f48deca8b774c63b5938997cdb833d41b31
sonycronic <sonycronic@gmail.com>
2023-03-13 20:17:31 +0600

    all

---
+++ num = int(input('Number: ')).py
@@ -0,0 +1,163 @@
+# num = int(input('Number: '))
+# if num % 2 == 0:    
+#     print(f'Число {num} четная' )
+# else:
+#     print(f'Число {num} нечетная')
+
+
+
+# a = 2**3 
+# b = 3**2 
+# if a > b:
+#     print(f'{a} больше чем {b}')
+# else:
+#     print(f'{a} меньше чем {b}')
+
+
+# N = range(0, 22) + range(57, 101)
+
+# number_to_check = 15
+
+# if number_to_check not in N:
+#     print("")
+
+
+# a = int(input('Number: '))
+# if a % 2 == 0:
+#     print
+# if a % 3 == 0:
+#     print 
+# if a ** 2 > 1000:
+#     print
+
+
+# a = 10//5
+# b = 10/5
+# if a == b:
+# else:
+#     print('Они не равны')
+
+# a = 'Hello'
+# b = "Hello"
+# c = """
+#     Hello
+# """
+# name = input('Введи имя')
+# print(name.lower()) # В нижний регистр 
+# print(name.title()) # Все начальные буквы в верхнем регистре 
+# print(name.upper()) # Все буквы в верхнем регистре 
+# print(namae.swapcase()) # Все большие в малеенький и ммаленткие в большой
+# print(name.capitalize()) # Начальную букву в верхний регистр
+
+
+# command = input("Напиши мне привет: ")
+# if command.upper() =="ПРИВЕТ":
+#     print(True)
+# else:
+#     print(False)
+
+
+#     name = input("Введи имя: ")
+#     print(name.isdigit()) #Все ли состоит из цифр
+#     print(name.isalpha()) #Все ли состоит из букв
+#     print(name.isidentifier()) #Все ли у нас пишется слитно 
+#     print(name.isupper()) #Все ли у нас из пишется из регистра
+#     print(name.islower()) 
+
+
+
+
+#     # print(len(name))
+# name = input("Введи имя: ")
+# if name.isalpha():
+#     if not(name.isspace()):
+#         print('Дальше')
+# else:
+#     pass
+
+
+# if name.isalpha():
+#     if len(name) > 5:
+#         if name.istitle():
+#             print('Все с именем хорошо')
+#         else:
+#             print('')
+
+
+
+# text = 'Hello World'
+# print(text.strip())
+# print(text.lstrip('H'))
+# print(text.rstrip('d'))
+
+# a = text.center(20,'*')
+# print(a)
+# print(len(a))
+
+# print(text)
+
+# text = 'https://www.google.com'
+# print('|*|'.join(text))
+
+
+password = (input('Введите пароль: '))
+password2 = input('Введите парол еще: ') 
+if password == password2:
+    print("Вход подтвержден.")
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+    
+
+
+
+
+
+
+
+
+
